238b3cbb4565871b3c84129c0f735f048ec00e56
Mehrdad Mehralian <dadwic@yahoo.com>
2024-10-18 15:52:50 +0300

    update DrawerAppBar menus

--- src/components/DrawerAppBar.js
+++ src/components/DrawerAppBar.js
@@ -3,49 +3,36 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
-import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import Toolbar from "@mui/material/Toolbar";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import Drawer from "@mui/material/SwipeableDrawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import PolicyIcon from "@mui/icons-material/Policy";
+import LiveHelpIcon from "@mui/icons-material/LiveHelp";
+import LanguageIcon from "@mui/icons-material/Language";
+import LiraIcon from "@mui/icons-material/CurrencyLira";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AddShoppingIcon from "@mui/icons-material/AddShoppingCart";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 
-const drawerWidth = 240;
-const navItems = ["Home", "About", "Contact"];
+const iOS =
+  typeof navigator !== "undefined" &&
+  /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-function DrawerAppBar(props) {
-  const { window } = props;
+function DrawerAppBar() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <>
@@ -78,22 +65,106 @@ function DrawerAppBar(props) {
       </AppBar>
       <nav>
         <Drawer
-          container={container}
+          dir="rtl"
+          anchor="right"
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
+          disableDiscovery={iOS}
+          disableBackdropTransition={!iOS}
           ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
+            keepMounted: true,
           }}
           sx={{
             display: { xs: "block", sm: "none" },
             "& .MuiDrawer-paper": {
               boxSizing: "border-box",
-              width: drawerWidth,
+              width: 240,
             },
           }}
         >
-          {drawer}
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton href="https://www.rialir.com/" target="_blank">
+                <ListItemIcon>
+                  <LanguageIcon />
+                </ListItemIcon>
+                <ListItemText primary="ریالیر" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton
+                href="https://www.rialir.com/lir/"
+                target="_blank"
+              >
+                <ListItemIcon>
+                  <LiraIcon />
+                </ListItemIcon>
+                <ListItemText primary="قیمت لیر ترکیه" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton
+                target="_blank"
+                href="https://www.rialir.com/account/"
+              >
+                <ListItemIcon>
+                  <AccountCircleIcon />
+                </ListItemIcon>
+                <ListItemText primary="حساب ریالیر" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton
+                target="_blank"
+                href="https://www.rialir.com/account/order/"
+              >
+                <ListItemIcon>
+                  <AddShoppingIcon />
+                </ListItemIcon>
+                <ListItemText primary="خرید از ترکیه" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton
+                target="_blank"
+                href="https://www.rialir.com/faq/"
+              >
+                <ListItemIcon>
+                  <LiveHelpIcon />
+                </ListItemIcon>
+                <ListItemText primary="سوالات متداول" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton
+                target="_blank"
+                href="https://www.rialir.com/privacy-policy/"
+              >
+                <ListItemIcon>
+                  <PolicyIcon />
+                </ListItemIcon>
+                <ListItemText primary="حریم خصوصی" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton
+                target="_blank"
+                href="https://www.rialir.com/contact/"
+              >
+                <ListItemIcon>
+                  <AlternateEmailIcon />
+                </ListItemIcon>
+                <ListItemText primary="تماس با ریالیر" />
+              </ListItemButton>
+            </ListItem>
+          </List>
         </Drawer>
       </nav>
     </>
