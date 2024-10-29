efab0fdfafebabd3389f7015be2da3995c3010b5
Mehrdad Mehralian <dadwic@yahoo.com>
2024-10-30 01:46:56 +0300

    add generateMetadata func: seo data

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { headers } from "next/headers";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
@@ -9,6 +10,26 @@ import LiraIcon from "@mui/icons-material/CurrencyLira";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import DrawerAppBar from "@/components/DrawerAppBar";
 import Domains from "@/components/Domains";
+import data from "@/components/data";
+
+export async function generateMetadata() {
+  const host = headers().get("host");
+  const current = data.find((v) => v.domain === host);
+  return {
+    title: current?.name
+      ? "ریالیر: خرید مستقیم محصولات اورجینال " +
+        current.name +
+        " از ترکیه بدون کارمزد! تحویل در ایران با تضمین اصالت کالا"
+      : "ریالیر: خرید از ترکیه بدون کارمزد | خرید از آمازون | نقد کردن درآمد یوتیوب",
+    description: current?.name
+      ? "خرید مستقیم از " +
+        current.name +
+        " ترکیه بدون کارمزد با ریالیر! محصولات اورجینال " +
+        current.name +
+        " را به صورت آنلاین و بدون کارمزد در سایت ریالیر سفارش دهید و با ضمانت اصالت کالا در ایران تحویل بگیرید."
+      : "ریالیر، پیشگام در خرید از ترکیه بدون کارمزد! تجربه خرید آنلاین محصولات اورجینال از برندهای معتبر و تحویل سریع در ایران با تضمین اصالت کالا. امکان نقد کردن درآمد یوتیوب بدون کارمزد برای کاربران ایرانی با خدمات سایت ریالیر",
+  };
+}
 
 export default function Home() {
   return (
