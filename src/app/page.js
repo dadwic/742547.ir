a9f5a7ef75b5970a319be25c575d972843baa6ac
Mehrdad Mehralian <dadwic@yahoo.com>
2024-11-02 23:49:44 +0300

    add en name to meta tags

--- src/app/page.js
+++ src/app/page.js
@@ -15,17 +15,21 @@ import data from "@/components/data";
 export async function generateMetadata() {
   const host = headers().get("host");
   const current = data.find((v) => v.domain === host);
+  const en_name = current?.en ? ` ${current.en} ` : "";
   return {
     title: current?.name
       ? "ریالیر: خرید مستقیم محصولات اورجینال " +
         current.name +
+        en_name +
         " از ترکیه بدون کارمزد! تحویل در ایران با تضمین اصالت کالا"
       : "ریالیر: خرید از ترکیه بدون کارمزد | خرید از آمازون | نقد کردن درآمد یوتیوب",
     description: current?.name
       ? "خرید مستقیم از " +
         current.name +
+        en_name +
         " ترکیه بدون کارمزد با ریالیر! محصولات اورجینال " +
         current.name +
+        en_name +
         " را به صورت آنلاین و بدون کارمزد در سایت ریالیر سفارش دهید و با ضمانت اصالت کالا در ایران تحویل بگیرید."
       : "ریالیر، پیشگام در خرید از ترکیه بدون کارمزد! تجربه خرید آنلاین محصولات اورجینال از برندهای معتبر و تحویل سریع در ایران با تضمین اصالت کالا. امکان نقد کردن درآمد یوتیوب بدون کارمزد برای کاربران ایرانی با خدمات سایت ریالیر",
     icons: {
